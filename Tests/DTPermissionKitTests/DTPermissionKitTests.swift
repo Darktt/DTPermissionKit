02367c83738aa29ee85d6f85a5b3f9bedf6a692c
Eden <edendudoo@gmail.com>
2021-08-18 11:01:23 +0800

    Upadate package.

---
+++ Tests/DTPermissionKitTests/DTPermissionKitTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import DTPermissionKit
+
+    final class DTPermissionKitTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(DTPermissionKit().text, "Hello, World!")
+        }
+    }
